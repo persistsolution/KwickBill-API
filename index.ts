3bb3103ae01876cc52f2eda0b3f1e8ccfae1ce2d
persistsolution <persistsolution1@gmail.com>
2025-01-10 17:53:37 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,32 +2,31 @@ require("dotenv").config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-//import multer from 'multer';
 // import userRoutes from '@routes/userRoutes';
 import categoryRoute from  './src/routers/billsoftadmin/selling-product/category-route';
 import subCategoryRoute from './src/routers/billsoftadmin/selling-product/subcategory-route';
-//import employeeRoute from './src/routers/billsoftadmin/employee/employee-route';
-//import productRoute from './src/routers/billsoftadmin/selling-product/product-route';
-//import franchiseRoute from './src/routers/billsoftadmin/franchise/franchise-route';
-import userRoute from './src/routers/billsoftadmin/users/user-route';
+import employeeRoute from './src/routers/billsoftadmin/employee/employee-route';
+import productRoute from './src/routers/billsoftadmin/selling-product/product-route';
+import franchiseRoute from './src/routers/billsoftadmin/franchise/franchise-route';
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(categoryRoute);
 app.use(subCategoryRoute);
-// app.use(employeeRoute);
-// app.use(productRoute);
-// app.use(franchiseRoute);
-app.use(userRoute);
-app.use(express.static('uploads'));
+app.use(employeeRoute);
+app.use(productRoute);
+app.use(franchiseRoute);
 
 // Enable CORS for specific origin
- app.use(cors({
+app.use(cors({
   origin: 'http://13.234.213.24:5173', // Allow requests from this origin
  }));
 
+ app.get("/", (req, res) => {
+  res.send("Welcome to the Node.js TypeScript server!");
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
