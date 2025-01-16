ddfbb8d552f04517bd9147f80ffaf60aa71eb498
persistsolution <persistsolution1@gmail.com>
2025-01-16 23:48:09 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ app.use(cors({
  }));
 
  app.get("/", (req, res) => {
-  res.send("Welcome to Kwickbill Development New");
+  res.send("Welcome to Kwickbill Development");
 });
 
 app.listen(port,'0.0.0.0', () => {
